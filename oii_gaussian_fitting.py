1d47534417a842bed2f960fd0d08317540b89a5b
orianashi <oshi@college.harvard.edu>
2026-07-02 12:39:57 -0400

    about to change bounds on amplitudes

--- oii_gaussian_fitting.py
+++ oii_gaussian_fitting.py
@@ -219,7 +219,7 @@ g_3726A.mean.tied = create_mean_tie(1, lineratio_A)
 g_3726A.stddev.tied = create_std_tie(1)
 g_3726A.amplitude.bounds = (0,
                             wing_bestfit.amplitude_0 / 1.1
-                            )  # ensures ratio 1.1 < 3729/3726 < 1.5 #wing_bestfit.amplitude_0 / 1.5
+                            )  # removed lower bound on amplitude 
 
 g_3729B = models.Gaussian1D(name=f'{line_names[1]}_B',
                             mean=lines[1] * (z_B + 1),
@@ -230,7 +230,7 @@ g_3729B.mean.tied = create_mean_tie(2, lineratio_B)
 g_3729B.stddev.tied = create_std_tie(2)
 g_3729B.amplitude.bounds = (0,
                             wing_bestfit.amplitude_1 * 1.5
-                            )  # ensures ratio 1.1 < 3729/3726 < 1.5 #wing_bestfit.amplitude_1 * 1.1
+                            )  # removed lower bound on amplitude, so now R < 1.5 
 
 # make the compound model
 compound_model = g_3726A + g_3729A + g_3726B + g_3729B + continuum
